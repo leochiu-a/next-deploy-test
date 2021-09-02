95b7445827fd46b595e5fd045ddd7bc42f8a9ab6
Leo <leochiu@hahow.in>
2021-09-02 21:57:30 +0800

    change title

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -11,7 +11,7 @@ interface HomeProps {
 const Home = ({ products }: HomeProps) => {
   return (
     <>
-      <PageTitle>商品列表</PageTitle>
+      <PageTitle>商品列表 React Refresh</PageTitle>
       <ProductGallery>
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
